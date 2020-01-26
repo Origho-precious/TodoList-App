47a287b2458005e911229d381c6d277c1480c585
Origho-precious <53418429+Origho-precious@users.noreply.github.com>
2020-01-26 19:50:29 +0100

    Added localStorage

--- index.js
+++ index.js
@@ -1,8 +1,67 @@
 var form = document.querySelector('#addForm');
 var itemList = document.querySelector('.list-group');
 
+
 // EventListener to form
 form.addEventListener('submit', addItem);
+
+// Add task To localStorage
+const storeTaskInLocalStorage = (task) => {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Get task from localStorage
+const getTaskFromLocalStorage = () => {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+        tasks.forEach((task) => {
+            var li = document.createElement('li');
+
+            // Add class to the created list item
+            li.className = 'list-group-item';
+
+            // Append new value to new listitem
+            li.appendChild(document.createTextNode(task));
+
+            // create delete button
+            var delBtn = document.createElement('button');
+
+            // create check button
+            var checkListBtn = document.createElement('button');
+
+            // Add class to delete button
+            delBtn.className = 'far fa-trash-alt delete';
+
+            // Add class to check button
+            checkListBtn.className = 'fas fa-check-circle check';
+
+            // Append delete button to li
+            li.appendChild(delBtn);
+
+            // Append check button to li
+            li.appendChild(checkListBtn);
+
+            // Add new  item to todo list
+            itemList.appendChild(li);
+        })
+}
+
+
+// DOM event that Restores tasks from LocalStorage
+document.addEventListener('DOMContentLoaded', getTaskFromLocalStorage);
 
 
 // Additem function
@@ -10,46 +69,50 @@ function addItem(e){
     e.preventDefault();
 
     // Get value of inputfield
-    var newItem = document.getElementById('add-Todo').value;
+    var newItem = document.getElementById('add-Todo');
 
-    //create a new listitem in the Ul
-
+    //CREATE A NEW ITEMLIST IN THE UI
     //checking if there is a text in inputfield befoe creating
    if(newItem.length == 0) {
-       var warning = 'Type in Todo in inputField!';
-        alert(warning);
+       alert('Type in Todo in inputField!');
    }else{
-      var li = document.createElement('li');
-   }
+        var li = document.createElement('li');
    
-    // Add class to the created list item
-    li.className ='list-group-item';
+        // Add class to the created list item
+        li.className ='list-group-item';
 
-    // Append new value to new listitem
-    li.appendChild(document.createTextNode(newItem));
+        // Append new value to new listitem
+        li.appendChild(document.createTextNode(newItem.value));
 
-    // create delete button
-    var delBtn = document.createElement('button');
+        // create delete button
+        var delBtn = document.createElement('button');
 
-    // create check button
-    var checkListBtn = document.createElement('button');
+        // create check button
+        var checkListBtn = document.createElement('button');
 
-    // Add class to delete button
-    delBtn.className = 'far fa-trash-alt delete';
+        // Add class to delete button
+        delBtn.className = 'far fa-trash-alt delete';
 
-    // Add class to check button
-    checkListBtn.className = 'fas fa-check-circle check';
+        // Add class to check button
+        checkListBtn.className = 'fas fa-check-circle check';
 
-    // Append delete button to li
-    li.appendChild(delBtn);
+        // Append delete button to li
+        li.appendChild(delBtn);
 
-    // Append check button to li
-    li.appendChild(checkListBtn);
+        // Append check button to li
+        li.appendChild(checkListBtn);
 
-    // Add new  item to todo list
-    itemList.appendChild(li);
+        // Add new item to todo list
+        itemList.appendChild(li);
+
+        // Add task to localStorage
+        storeTaskInLocalStorage(newItem.value);
+
+        // Clear fields
+        newItem.value = '';
+
+   }
 }
-
 
  
 // EventListener for delete button
@@ -60,7 +123,28 @@ function removeItem(e){
     if(e.target.classList.contains('delete')){
         var li = e.target.parentElement;
         itemList.removeChild(li);
-  }
+    }
+
+    removeFromLocalStorage(e.target.parentElement);
+    
+}
+
+function removeFromLocalStorage(tsk){
+    // Delete from localStorage
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach((task, index) => {
+        if (tsk.textContent === task) {
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 
